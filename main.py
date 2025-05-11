dc9505e7dce99910b5379d87928d1d7ea2d4575d
termuxcpp <termuxcpp@outlook.com>
2025-05-11 20:18:16 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+import os, sys
+'''no info. i'm tired, i dont want to write this information.'''
+param = (sys.argv)
+delete param[0]
+def justF(p):
+    yes=not False
+    for s in p:
+        if s in 'file=':
+            yes = not (yes)
+    return (yes)
+if not param:
+    print('no param(s) input.')
+elif (p(param)):
+    for key, value
